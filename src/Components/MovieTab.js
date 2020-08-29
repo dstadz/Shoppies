9d91069e40f04f1aa71caa5b9ac2e9aacd81c448
Daniel Stadler <daniel.stadler.x@gmail.com>
2020-08-29 14:27:09 -0700

    nominate btn reenabled when removed from nom list

--- src/Components/MovieTab.js
+++ src/Components/MovieTab.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { useRecoilState } from 'recoil'
+import { useRecoilValue, useRecoilState } from 'recoil'
 
-import { nominatedState } from '../Utils/store'
+import { searchResultState, nominatedState } from '../Utils/store'
 import { Tab } from '../styles/'
 
 const MovieTab = ({props}) => {
   const { Title, Year, imdbID,/* Type, Poster,*/ action } = props
+  const searchResults = useRecoilValue(searchResultState)
   const [nominated, setNominated] = useRecoilState(nominatedState)
   const [isNominated, setIsNominated] = useState(false)
 
 
 
   const handleClick = () => {
-
     if (action === 'Nominate'
     && nominated.length < 5
     && !nominated.some(movie => movie.imdbID === imdbID)) {
@@ -20,15 +20,18 @@ const MovieTab = ({props}) => {
       setIsNominated(true)
     }
 
-
     if (action === 'Remove') {
       setIsNominated(false)
       const newList = nominated.filter((movie) => imdbID !== movie.imdbID)
+      console.log(newList, nominated)
       setNominated(newList)
     }
-    console.log(nominated)
   }
 
+  useEffect(() => {
+    if (!nominated.some(movie => movie.imdbID === imdbID))
+    setIsNominated(false)
+  }, [nominated])
 
 
   return (
@@ -36,8 +39,7 @@ const MovieTab = ({props}) => {
       <h5>{Title}</h5>
       <span>{Year}</span>
       <button
-      id={imdbID}
-      disabled={isNominated}
+      disabled={isNominated && (action ==='Nominate')}
       onClick={()=> handleClick()}
       >{action}</button>
     </Tab>
